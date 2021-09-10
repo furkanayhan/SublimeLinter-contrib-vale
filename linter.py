a34ae99139d2a1b14e2868f0bbb75614e5ff7406
Furkan Ayhan <furkanayhn@gmail.com>
2021-09-10 16:04:12 +0300

    Update linter.py

--- linter.py
+++ linter.py
@@ -10,10 +10,10 @@
 
 """This module exports the Vale plugin class."""
 
-from SublimeLinter.lint import Linter, util
+from SublimeLinter.lint import NodeLinter, util
 
 
-class Vale(Linter):
+class Vale(NodeLinter):
     """Provides an interface to vale."""
 
     defaults = {
@@ -22,15 +22,16 @@ class Vale(Linter):
     # cmd = 'vale --no-wrap'
     cmd = ('vale', '${args}', '${temp_file}')
     # executable = None
-    version_args = '--version'
-    version_re = r'(?P<version>\d+\.\d+\.\d+)'
-    version_requirement = '>= 0.5.0'
-    regex = (
-        r'(?P<line>\d+):(?P<col>\d+)\s{2,}'
-        r'((?P<error>error)|(?P<warning>warning))\s{2,}'
-        r'(?P<message>.+?)(?=$)'
-    )
-    multiline = True
+    # version_args = '--version'
+    # version_re = r'(?P<version>\d+\.\d+\.\d+)'
+    # version_requirement = '>= 0.5.0'
+    #regex = (
+    #    r'(?P<line>\d+):(?P<col>\d+)\s{2,}'
+    #    r'((?P<error>error)|(?P<warning>warning))\s{2,}'
+    #    r'(?P<message>.+?)(?=$)'
+    #)
+    regex = r'.+?(?:[:](?P<line>\d+))(?:[:](?P<col>\d+))?\s+(?P<error>MD\d+)?[/]?(?P<message>.+)'
+    multiline = False
     line_col_base = (1, 1)
     tempfile_suffix = 'md'
     # error_stream = util.STREAM_BOTH

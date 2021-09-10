ec31c56b918a111435fddb5f697e91900c9f2cc2
Furkan Ayhan <furkanayhn@gmail.com>
2021-09-10 16:17:17 +0300

    Update linter.py

--- linter.py
+++ linter.py
@@ -19,24 +19,14 @@ class Vale(NodeLinter):
     defaults = {
         'selector': 'text.plain, text.html.markdown',
     }
-    # cmd = 'vale --no-wrap'
     cmd = ('vale', '--no-wrap', '${args}', '${temp_file}')
-    # executable = None
-    # version_args = '--version'
-    # version_re = r'(?P<version>\d+\.\d+\.\d+)'
-    # version_requirement = '>= 0.5.0'
     regex = (
         r'(?P<line>\d+):(?P<col>\d+)\s{2,}'
         r'((?P<error>error)|(?P<warning>warning))\s{2,}'
         r'(?P<message>.+?)(?=$)'
     )
-    # regex = r'.+?(?:[:](?P<line>\d+))(?:[:](?P<col>\d+))?\s+(?P<error>MD\d+)?[/]?(?P<message>.+)'
     multiline = True
     line_col_base = (1, 1)
     tempfile_suffix = 'md'
     error_stream = util.STREAM_BOTH
-    # error_stream = util.STREAM_STDERR
     word_re = None
-    # inline_settings = None
-    # inline_overrides = None
-    # comment_re = None
